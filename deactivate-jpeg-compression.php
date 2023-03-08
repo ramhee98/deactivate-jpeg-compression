0a42e5be45970e236c2026f125a244c402a84ed7
ramhee98 <heeb-ramon@hotmail.com>
2023-03-08 14:24:48 +0100

    initial version

---
+++ deactivate-jpeg-compression.php
@@ -0,0 +1,15 @@
+<?php
+/**
+* Plugin Name: Deactivate JPEG Compression
+* Plugin URI: https://github.com/ramhee98/deactivate-jpeg-compression
+* Description: This Plugin deactivates JPEG compression.
+* Version: 1.0
+* Author: ramhee
+* Author URI: https://ramhee.ch/
+* License: GPL-3.0
+* License URI: https://github.com/ramhee98/deactivate-jpeg-compression/blob/main/LICENSE
+*/
+
+/***** this deactivates picture compression ;) *****/
+
+add_filter('jpeg_quality', function($arg){return 100;}); add_filter( 'wp_editor_set_quality', function($arg){return 100;} );
